8f3ea4f05ca408d20e4d98fbc84946fd3a542c58
thinkpad <lihairuizone@sina.com>
2019-02-19 11:04:39 +0800

    peasonal pc

---
+++ springboot01/src/main/java/cn/hairui/spring/springboot/SpringBootMain.java
@@ -0,0 +1,17 @@
+package  cn.hairui.spring.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lihairui
+ * @version V1.0
+ * @date: 2019/2/18 22:54
+ */
+@SpringBootApplication
+public class SpringBootMain {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringBootMain.class,args);
+    }
+}
---
+++ springboot01/src/main/java/cn/hairui/spring/springboot/controller/TestController.java
@@ -0,0 +1,21 @@
+package cn.hairui.spring.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author lihairui
+ * @version V1.0
+ * @date: 2019/2/18 22:58
+ */
+@Controller
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
